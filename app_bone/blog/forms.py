362d5b42de6dc4e6543b4136f47d66d7cc7851f6
poya shadman <poya_kob@live.com>
2022-02-07 18:16:42 +0330

    send for debug

--- app_bone/blog/forms.py
+++ app_bone/blog/forms.py
@@ -2,7 +2,10 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms import StringField
 from wtforms_sqlalchemy.fields import QuerySelectField
+from wtforms.validators import ValidationError
 from flask_ckeditor import CKEditorField
+
+from .models import Blog
 
 
 class AddPost(FlaskForm):
@@ -10,6 +13,11 @@ class AddPost(FlaskForm):
     image = FileField(label='', validators=[])
     body = CKEditorField(label='', validators=[])
     category = QuerySelectField(label='', validators=[], get_label='name')
+
+    def validate_title(self, title_to_check):
+        blog = Blog.query.filter_by(title=title_to_check.data).first()
+        if blog:
+            raise ValidationError('عنوان تکراری است ')
 
 
 class AddCat(FlaskForm):
